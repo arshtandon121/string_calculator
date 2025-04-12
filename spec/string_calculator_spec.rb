e11656e42a5df8a8f1557c076fecf838215c634a
arshdeeptandon <arshdeep.tandon@planetspark.in>
2025-04-12 09:11:13 +0530

    Handle single number input in add method

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,11 +1,18 @@
 require 'string_calculator'
 
 describe StringCalculator do
-  describe '.add' do
-    context 'given an empty string' do
-      it 'returns zero' do
-        expect(StringCalculator.add("")).to eq(0)
-      end
+    describe '.add' do
+        context 'given an empty string' do
+        it 'returns zero' do
+            expect(StringCalculator.add("")).to eq(0)
+        end
+        end
+
+        context 'given a single number' do
+            it 'returns the number' do
+            expect(StringCalculator.add("1")).to eq(1)
+            end
+        end
+
     end
-  end
 end
